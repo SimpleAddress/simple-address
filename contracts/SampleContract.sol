05332b90e12b6c5aea5d323d2f2600c2f44549a5
Omar <omardraz94@gmail.com>
2022-01-22 12:04:17 -0800

    skeleton of contract

--- contracts/SampleContract.sol
+++ contracts/SampleContract.sol
@@ -2,10 +2,59 @@
 
 pragma solidity ^0.8.0;
 
-contract SampleContract {
-    event Greet(string message);
+contract SimpleAddressCore {
+    address[] public owners;
+    bool public pause;
 
-    function greet() public {
-        emit Greet("Hello World!");
+    struct addrStat {
+        uint256 regTimestamp;
+        uint256 balance;
+        address[] tokens;
+        uint256 lastUpdatedTimestamp;
+        uint256 firstTxnTimestamp;
+    }
+
+    struct metaStat {
+        address addrStat;
+    }
+
+    struct connection {
+        address addr;
+        bool verified;
+    }
+
+    mapping(string => address) addressBook;
+    mapping(address => string) reverseAddressBook;
+    mapping(address => connection[]) metaToSub;
+    mapping(address => connection[]) subToMeta;
+    mapping(address => addrStat) addrStats;
+    mapping(address => metaStat) metaStats;
+
+    modifier isPaused() {
+        _;
+    }
+
+    modifier isUnique() {
+        _;
+    }
+
+    function getSimpleName() public {
+        //TODO
+    }
+
+    function addConnection() public {
+        //TODO
+    }
+
+    function verifyMeta() public {
+        //TODO
+    }
+
+    function verifySub() public {
+        //TODO
+    }
+
+    function viewAccount() public {
+        //TODO
     }
 }
